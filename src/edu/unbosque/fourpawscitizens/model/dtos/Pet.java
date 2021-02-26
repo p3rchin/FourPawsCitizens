7de9bfe03316a00f13b217be7460f2d21b6eb725
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-02-25 22:53:05 -0500

    Completing Pet class

--- src/edu/unbosque/fourpawscitizens/model/dtos/Pet.java
+++ src/edu/unbosque/fourpawscitizens/model/dtos/Pet.java
@@ -13,4 +13,71 @@ public class Pet {
         public Pet() {
 
         }
+
+    public Pet(String id, Long microchip, String species, String sex, String size, boolean potentDangerous, String neighborhood) {
+
+        this.id = id;
+        this.microchip = microchip;
+        this.species = species;
+        this.sex = sex;
+        this.size = size;
+        this.potentDangerous = potentDangerous;
+        this.neighborhood = neighborhood;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getMicrochip() {
+        return microchip;
+    }
+
+    public void setMicrochip(long microchip) {
+        this.microchip = microchip;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public boolean getPotentDangerous() {
+        return potentDangerous;
+    }
+
+    public void setPotentDangerous(boolean potentDangerous) {
+        this.potentDangerous = potentDangerous;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
 }
